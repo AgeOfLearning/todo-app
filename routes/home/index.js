5589bebff42ec7edeed2fe0bafb0d4cb459cbe5c
Arian Khosravi <arian.khosravi@aofl.com>
2019-01-03 10:00:36 -0800

    first commit

---
+++ routes/home/index.js
@@ -0,0 +1,42 @@
+/**
+ * @route /
+ * @title AofL::Home
+ * @prerender false
+ */
+import {template} from './template';
+import AoflElement from '@aofl/web-components/aofl-element';
+import styles from './template.css';
+
+/**
+ *
+ * @extends {AoflElement}
+ */
+class HomePage extends AoflElement {
+  /**
+   *
+   * Creates an instance of HomePage.
+   */
+  constructor() {
+    super();
+  }
+
+  /**
+   *
+   * @readonly
+   */
+  static get is() {
+    return 'home-page';
+  }
+
+  /**
+   *
+   * @return {Object}
+   */
+  render() {
+    return super.render(template, [styles]);
+  }
+}
+
+customElements.define(HomePage.is, HomePage);
+
+export default HomePage;
