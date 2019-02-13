0bb2c8e041dadde7b5a4caa7e989a4ccb868126c
Micah.Blu <micah.blu@aofl.com>
2019-02-12 18:05:51 -0800

    Updates reference to tutorial as readme only

--- routes/home/template.js
+++ routes/home/template.js
@@ -2,7 +2,7 @@ import '../../modules/link-to-element';
 
 export default (context, html) => html`
   <h1>Welcome to the AofL JS Todo app tutorial</h1>
-  <p>This repo provides source code for the todo app tutorial <a href="https://ageoflearning.github.io/aofl/#/v1.x/todo-app/index">here</a>. It is also provided in this repo's <b>README.md</b>.<br>
+  <p>This repo provides source code for the todo app tutorial in the <b>README.md</b>.<br>
   Below are links to the state of the app in each stage of the tutorial</p>
   <ul>
     <li><link-to href="/step-1">Step one (Naive approach)</link-to></li>
